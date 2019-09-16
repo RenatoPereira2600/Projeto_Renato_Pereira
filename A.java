092f4df67e64b985f32008fa2d440b369b430884
renatopereira2600 <renatopereira2600@gmail.com>
2019-09-16 19:37:30 -0300

    Classes A e B

---
+++ A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class A {
+    public A() { this.A1 = 0; this.A2 = 0; }
+    public void setA1(int A1) { this.A1 = A1; }
+    public int getA1() { return this.A1; }
+    public void setA2(float A2) { this.A2 = A2; }
+    public float getA2() { return this.A2; }
+    public void MA1() { System.out.println("MA1"); }
+    public void MA2() { System.out.println("MA2"); }
+    private int A1;
+    private float A2;
+}
---
+++ B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class B {
+    public B() { this.B1 = 0; this.B2 = 0; }
+    public void setA1(int B1) { this.B1 = B1; }
+    public int getA1() { return this.B1; }
+    public void setA2(float B2) { this.B2 = B2; }
+    public float getA2() { return this.B2; }
+    public void MB1() { System.out.println("MB1"); }
+    public void MB2() { System.out.println("MB2"); }
+    public int B1;
+    public float B2;
+}
