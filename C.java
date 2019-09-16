e2783ed18ac79384249c2dd773db32d27561e5d3
renatopereira2600 <renatopereira2600@gmail.com>
2019-09-16 19:59:48 -0300

    Criação da classe C

---
+++ C.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class C {
+    private int B1
+    private float B2
+    public void MC1() { System.out.println("MC1"); }
+    public void MC2() { System.out.println("MC2"); }
+}
