e05a06f5dc159d8d0738153635387b53e1196647
renatopereira2600 <renatopereira2600@gmail.com>
2019-09-16 20:28:00 -0300

    Simulando o conflito

--- D.java
+++ D.java
@@ -5,4 +5,5 @@ public class D {
     private float D2
     public void MD1() { System.out.println("MD1"); }
     public void MD2() { System.out.println("MD2"); }
+    public void MD3() { System.out.println("MD3"); }
 }
